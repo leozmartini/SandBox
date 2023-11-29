1138ddc3dff737f5d1d5d650f379d9aa9f38562e
Leonardo de Martini <demartinileo@hotmail.com>
2023-11-29 20:46:20 -0300

    resolvido bug de comprar quantidades decimais

--- js/app.js
+++ js/app.js
@@ -6,24 +6,29 @@ function comprar() {
     let select = document.getElementById('tipo-ingresso');
     let tipoIngresso = select.options[select.selectedIndex].value;
 
-    let quantidade = document.getElementById('qtd').value
-    if (quantidade == '') quantidade = 1
+    let quantidade = Number(document.getElementById('qtd').value)
+    if (quantidade == '') quantidade = 1;
 
+
+    if(!Number.isInteger(quantidade)) return alert('A quantidade deve ser um número inteiro.')
 
     if (tipoIngresso == 'inferior') {
         if (disponivelInferior - quantidade < 0) return alert('Esgotados');
+        alert(`Compra de ${quantidade} ingresso(s) efetuada.`)
 
         disponivelInferior -= quantidade; document.getElementById('qtd-inferior').textContent = disponivelInferior
     }
 
     if (tipoIngresso == 'superior') {
         if (disponivelSuperior - quantidade < 0) return alert('Esgotados');
+        alert(`Compra de ${quantidade} ingresso(s) efetuada.`)
 
         disponivelSuperior -= quantidade; document.getElementById('qtd-superior').textContent = disponivelSuperior
     }
 
     if (tipoIngresso == 'pista') {
         if (disponivelPista - quantidade < 0) return alert('Esgotados');
+        alert(`Compra de ${quantidade} ingresso(s) efetuada.`)
 
         disponivelPista -= quantidade; document.getElementById('qtd-pista').textContent = disponivelPista
     }
