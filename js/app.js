d336fc0de3fe661889def0e1e90a67ac798d3cc2
Leonardo de Martini <demartinileo@hotmail.com>
2023-11-30 00:15:41 -0300

    corrigido bug nas options

--- js/app.js
+++ js/app.js
@@ -1,39 +1,46 @@
 // contantes do HTML
 const totalElement = document.getElementById('valor-total');
-const produto = document.getElementById('produto').value;
 const produtosCarrinho = document.getElementById('lista-produtos');
 const total = document.getElementById('valor-total')
+
+const produtoElement = document.getElementById('produto')
+let produto
+
+
 
 
 // Valor inicial do carrinho
 let valorTotal = parseInt(totalElement.textContent.split('R$')[1])
 
 function adicionar() {
-    // Busca dados dos produtos
-    let nomeProduto = produto.split('-')[0]
-    let precoUnitario = produto.split('R$')[1]
-    let qtde = document.getElementById('quantidade').value;
-    if (!qtde) qtde = 1
+  // Armazena option selecionada
+  produto = produtoElement.options[produtoElement.selectedIndex].value;
 
-    // Calcula valor a pagar por preço * quantidade
-    let valorPagar = parseInt(precoUnitario * qtde)
+  // Busca dados dos produtos
+  let nomeProduto = produto.split('-')[0]
+  let precoUnitario = produto.split('R$')[1]
+  let qtde = document.getElementById('quantidade').value;
+  if (!qtde) qtde = 1
 
-    // Insere no carrinho texto do produto
-    produtosCarrinho.innerHTML += `<section class="carrinho__produtos__produto">
+  // Calcula valor a pagar por preço * quantidade
+  let valorPagar = parseInt(precoUnitario * qtde)
+
+  // Insere no carrinho texto do produto
+  produtosCarrinho.innerHTML += `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${qtde}x</span> ${nomeProduto} <span class="texto-azul">R$${valorPagar}</span>
   </section>`
 
-    // Calcula novo valor do carrinho
-    valorTotal += valorPagar
-    totalElement.textContent = `${valorTotal}`
+  // Calcula novo valor do carrinho
+  valorTotal += valorPagar
+  totalElement.textContent = `${valorTotal}`
 
-    // Limpa quantidade
-    document.getElementById('quantidade').value = null
+  // Limpa quantidade
+  document.getElementById('quantidade').value = null
 
 }
 
 function limpar() {
-    valorTotal = 0
-    produtosCarrinho.textContent = '' // Limpa produtos
-    total.textContent = 'R$00,00' // limpa preço
+  valorTotal = 0
+  produtosCarrinho.textContent = '' // Limpa produtos
+  total.textContent = 'R$00,00' // limpa preço
 }
